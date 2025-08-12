76cfd4a5ba2f5a69a26541300ca1497070bab7bf
Morph <morphwr@gmail.com>
2025-08-12 19:00:02 +0100

    prettier fix now after global pretter config file matches local project

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,8 @@ import './index.css';
 
 const container = document.getElementById('root');
 if (container) {
-    const root = createRoot(container);
-    root.render(<App />);
+  const root = createRoot(container);
+  root.render(<App />);
 } else {
-    console.error('Root element not found');
+  console.error('Root element not found');
 }
